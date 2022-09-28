4e6673f41720e8d45d5de889dbaa22b36fab7bcc
wy-luke <li1078797083@qq.com>
2022-09-28 20:21:16 +0800

    high dpi support

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 #include <QTranslator>
 
 int main(int argc, char *argv[]) {
+    //  High DPI
+    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1");
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+
     QApplication a(argc, argv);
 
     QTranslator translator;
