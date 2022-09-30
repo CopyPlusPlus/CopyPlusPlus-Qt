3d2778a6c9fe0a0d7963f38e561439e93d768ee5
wy-luke <li1078797083@qq.com>
2022-09-30 20:36:09 +0800

    更新界面

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,10 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    setFixedSize(420, 360);
+    ui->toggle1->setName("自动合并");
+    ui->toggle2->setName("快捷键合并");
 }
 
 MainWindow::~MainWindow() {
