44898f23d0ed2042e5fca297fc49cab3c577d83c
wy-luke <li1078797083@qq.com>
2023-01-10 19:54:46 +0800

    解决 High DPI 缩放图标模糊

--- main.cpp
+++ main.cpp
@@ -6,8 +6,10 @@
 int main(int argc, char *argv[])
 {
     //  High DPI
-    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1");
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+    // qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1");
+    // QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 
     QApplication a(argc, argv);
 
