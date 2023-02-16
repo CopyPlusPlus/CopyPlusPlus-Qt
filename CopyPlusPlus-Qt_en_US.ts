f9f28635304d144720344e263d625701a49d4e58
wy-luke <li1078797083@qq.com>
2023-02-16 09:06:54 +0800

    WIP:Add translation

--- CopyPlusPlus-Qt_en_US.ts
+++ CopyPlusPlus-Qt_en_US.ts
@@ -1,3 +1,100 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US"></TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="32"/>
+        <source>自动触发</source>
+        <translation>Auto</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="73"/>
+        <source>快捷键</source>
+        <translation type="unfinished">Hotkey</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="63"/>
+        <source>Mac 暂不支持自动合并</source>
+        <translation type="unfinished">Mac not supported</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="150"/>
+        <source>快捷键占用，请更换快捷键。</source>
+        <translation type="unfinished">Hotkey busy, change another.</translation>
+    </message>
+</context>
+<context>
+    <name>QHotkey</name>
+    <message>
+        <location filename="3rdparty/QHotkey/qhotkey.cpp" line="294"/>
+        <source>Failed to register %1. Error: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="3rdparty/QHotkey/qhotkey.cpp" line="314"/>
+        <source>Failed to unregister %1. Error: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QKeySequenceEdit</name>
+    <message>
+        <location filename="mykeysequenceedit.cpp" line="20"/>
+        <location filename="mykeysequenceedit.cpp" line="89"/>
+        <source>快捷键</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWindow</name>
+    <message>
+        <location filename="settingswindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingswindow.ui" line="27"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="9"/>
+        <source>设置</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="11"/>
+        <source>语言</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="13"/>
+        <source>中文</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="13"/>
+        <source>英文</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Toggle</name>
+    <message>
+        <location filename="toggle.ui" line="19"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="toggle.ui" line="43"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
