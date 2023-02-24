b58980689a80d20a1dad60d0c3e15a78ec53727f
wy-luke <li1078797083@qq.com>
2023-02-24 21:21:11 +0800

    Add languages folder

--- languages/CopyPlusPlus-Qt_en_US.ts
+++ languages/CopyPlusPlus-Qt_en_US.ts
@@ -4,28 +4,28 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.cpp" line="144"/>
+        <location filename="../src/mainwindow.cpp" line="114"/>
         <source>自动触发</source>
         <translation>Auto</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="145"/>
-        <location filename="mainwindow.cpp" line="146"/>
+        <location filename="../src/mainwindow.cpp" line="115"/>
+        <location filename="../src/mainwindow.cpp" line="116"/>
         <source>快捷键</source>
         <translation>Shortcut</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="115"/>
+        <location filename="../src/mainwindow.cpp" line="105"/>
         <source>Mac 暂不支持自动合并</source>
         <translation>Mac not supported yet</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="142"/>
+        <location filename="../src/mainwindow.cpp" line="112"/>
         <source>CopyPlusPlus</source>
         <translation>CopyPlusPlus</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="236"/>
+        <location filename="../src/mainwindow.cpp" line="206"/>
         <source>快捷键占用，请更换快捷键。</source>
         <translation>Shortcut occupied, please change another.</translation>
     </message>
@@ -33,8 +33,8 @@
 <context>
     <name>MyKeySequenceEdit</name>
     <message>
-        <location filename="mykeysequenceedit.cpp" line="45"/>
-        <location filename="mykeysequenceedit.cpp" line="65"/>
+        <location filename="../src/mykeysequenceedit.cpp" line="45"/>
+        <location filename="../src/mykeysequenceedit.cpp" line="65"/>
         <source>快捷键</source>
         <translation>Hotkey</translation>
     </message>
@@ -42,12 +42,12 @@
 <context>
     <name>QHotkey</name>
     <message>
-        <location filename="3rdparty/QHotkey/qhotkey.cpp" line="294"/>
+        <location filename="../3rdparty/QHotKey/qhotkey.cpp" line="294"/>
         <source>Failed to register %1. Error: %2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="3rdparty/QHotkey/qhotkey.cpp" line="314"/>
+        <location filename="../3rdparty/QHotKey/qhotkey.cpp" line="314"/>
         <source>Failed to unregister %1. Error: %2</source>
         <translation type="unfinished"></translation>
     </message>
@@ -55,8 +55,8 @@
 <context>
     <name>QKeySequenceEdit</name>
     <message>
-        <location filename="mykeysequenceedit.cpp" line="20"/>
-        <location filename="mykeysequenceedit.cpp" line="89"/>
+        <location filename="../src/mykeysequenceedit.cpp" line="20"/>
+        <location filename="../src/mykeysequenceedit.cpp" line="89"/>
         <source>快捷键</source>
         <translation>Hotkey</translation>
     </message>
@@ -64,12 +64,12 @@
 <context>
     <name>SettingsWindow</name>
     <message>
-        <location filename="settingswindow.cpp" line="51"/>
+        <location filename="../src/settingswindow.cpp" line="52"/>
         <source>设置</source>
         <translation>Settings</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="53"/>
+        <location filename="../src/settingswindow.cpp" line="53"/>
         <source>语言</source>
         <translation>Language</translation>
     </message>
