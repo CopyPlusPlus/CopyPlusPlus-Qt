4c73a08d71a669fa4aab13bb087c9bffb107018b
wy-luke <li1078797083@qq.com>
2023-04-19 21:56:57 +0800

    Move to cmake

---
+++ src/components/toggle.h
@@ -0,0 +1,36 @@
+#ifndef TOGGLE_H
+#define TOGGLE_H
+
+#include <QWidget>
+
+class QtMaterialToggle;
+
+namespace Ui {
+    class Toggle;
+}
+
+class Toggle : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Toggle(QWidget *parent = nullptr);
+    explicit Toggle(QString _name, QWidget *parent = nullptr);
+    ~Toggle();
+    bool isChecked();
+    void toggle();
+    void setChecked(bool status);
+    QString getName();
+    void setName(QString name);
+
+private:
+    void initToggle();
+
+public:
+    QtMaterialToggle *m_toggle;
+
+private:
+    Ui::Toggle *ui;
+};
+
+#endif // TOGGLE_H
---
+++ src/ui/settingswindow.cpp
@@ -0,0 +1,70 @@
+#include "settingswindow.h"
+#include "mainwindow.h"
+#include "qevent.h"
+#include "ui_settingswindow.h"
+#include "utils/language.h"
+
+#include <QSettings>
+
+SettingsWindow::SettingsWindow(QWidget *parent) : QMainWindow(parent),
+                                                  ui(new Ui::SettingsWindow)
+{
+    ui->setupUi(this);
+
+    setFixedSize(260, 295);
+    setWindowFlags(Qt::Tool);
+
+    updateText();
+
+    QSettings settings;
+    ui->languageList->setCurrentIndex(settings.value("language", "0").toInt());
+
+    // 函数指针
+    void (QComboBox::*currentIndexChanged)(int) = &QComboBox::currentIndexChanged;
+    connect(ui->languageList, currentIndexChanged, MainWindow::getInstance(), &Language::updateLanguage);
+}
+
+SettingsWindow::~SettingsWindow()
+{
+    delete ui;
+}
+
+void SettingsWindow::closeEvent(QCloseEvent *event)
+{
+    emit closed();
+    event->accept();
+}
+
+void SettingsWindow::changeEvent(QEvent *event)
+{
+    if (event->type() == QEvent::LanguageChange) {
+        // 感觉没啥用
+        ui->retranslateUi(this);
+
+        // 更新文本，用于翻译
+        updateText();
+    }
+
+    QMainWindow::changeEvent(event);
+}
+
+void SettingsWindow::updateText()
+{
+    setWindowTitle(tr("设置"));
+    ui->languageLable->setText(tr("语言"));
+
+    // Get QComboBox
+    QComboBox *list = ui->languageList;
+
+    QStringList allLanguages = Language::allLanguages;
+
+    if (list->count() == 0) {
+        for (int i = 0; i < allLanguages.count(); ++i) {
+            list->addItem(tr(allLanguages[i].toUtf8()));
+        }
+    } else {
+        for (int i = 0; i < allLanguages.count(); ++i) {
+            list->setItemText(i, tr(allLanguages[i].toUtf8()));
+        }
+    }
+}
