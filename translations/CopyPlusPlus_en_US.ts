e3e3410b58cefea4232fb0cd64c89913b41a8912
wy-luke <li1078797083@qq.com>
2023-04-28 22:27:22 +0800

    Add translation

---
+++ translations/CopyPlusPlus_en_US.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>自动触发</source>
+        <translation type="vanished">Auto</translation>
+    </message>
+    <message>
+        <source>快捷键</source>
+        <translation type="vanished">Shortcut</translation>
+    </message>
+    <message>
+        <source>Mac 暂不支持自动合并</source>
+        <translation type="vanished">Mac not supported yet</translation>
+    </message>
+    <message>
+        <source>CopyPlusPlus</source>
+        <translation type="vanished">CopyPlusPlus</translation>
+    </message>
+    <message>
+        <source>快捷键占用，请更换快捷键。</source>
+        <translation type="vanished">Shortcut occupied, please change another.</translation>
+    </message>
+</context>
+<context>
+    <name>MyKeySequenceEdit</name>
+    <message>
+        <source>快捷键</source>
+        <translation type="vanished">Hotkey</translation>
+    </message>
+</context>
+<context>
+    <name>QKeySequenceEdit</name>
+    <message>
+        <source>快捷键</source>
+        <translation type="vanished">Hotkey</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWindow</name>
+    <message>
+        <source>设置</source>
+        <translation type="vanished">Settings</translation>
+    </message>
+    <message>
+        <source>语言</source>
+        <translation type="vanished">Language</translation>
+    </message>
+</context>
+</TS>
