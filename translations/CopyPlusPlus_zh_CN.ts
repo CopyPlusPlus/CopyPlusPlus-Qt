66e3162f36ca4034f929527bb925a677c665e476
wy-luke <li1078797083@qq.com>
2023-05-17 00:12:15 +0800

    Update translation settings

---
+++ translations/CopyPlusPlus_zh_CN.ts
@@ -0,0 +1,90 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="14"/>
+        <location filename="../src/ui/mainwindow.cpp" line="108"/>
+        <source>CopyPlusPlus</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="102"/>
+        <location filename="../src/ui/mainwindow.ui" line="138"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="109"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="129"/>
+        <source>AppMenu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.cpp" line="76"/>
+        <location filename="../src/ui/mainwindow.cpp" line="110"/>
+        <source>Auto Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.cpp" line="77"/>
+        <location filename="../src/ui/mainwindow.cpp" line="111"/>
+        <source>Hotkey Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.cpp" line="210"/>
+        <source>Hotkey occupied, change it.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MyKeySequenceEdit</name>
+    <message>
+        <location filename="../src/components/mykeysequenceedit.cpp" line="45"/>
+        <location filename="../src/components/mykeysequenceedit.cpp" line="65"/>
+        <source>Hotkey</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QKeySequenceEdit</name>
+    <message>
+        <location filename="../src/components/mykeysequenceedit.cpp" line="20"/>
+        <location filename="../src/components/mykeysequenceedit.cpp" line="89"/>
+        <source>Hotkey</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWindow</name>
+    <message>
+        <location filename="../src/ui/settingswindow.ui" line="14"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/settingswindow.ui" line="27"/>
+        <source>Languages</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Toggle</name>
+    <message>
+        <location filename="../src/components/toggle.ui" line="20"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/components/toggle.ui" line="38"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
